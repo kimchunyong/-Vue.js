7568a71700beafa63a37ee5a63165a9b811c3f05
Kimchunyong <lovemy5854@naver.com>
2018-12-18 19:06:21 +0900

    TabView.js에서 오류발생 debugger

---
+++ vue-practice/js/views/TabView.js
@@ -0,0 +1,18 @@
+import View from './View.js';
+
+const tag = '[TabView]';
+
+const TabView = Object.create(View);
+
+TabView.setup = function (el) {
+    this.init(el);
+}
+
+TabView.setActiveTab = function (tabName) {
+    debugger
+    Array.from(this.el.querySelectorAll('li').forEach(li => {
+        li.className = li.innerHTML === tabName ? 'active' : '';
+    }))
+}
+
+export default TabView;
